64475cc37a0ab6041406e1af39a01195837ec97b
김민서 <kimminseo@gimminseoui-MacBookPro.local>
2022-12-05 17:58:58 +0900

    ADD : 초기세팅 완료

---
+++ src/Router.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Nav from "./components/Nav/Nav";
+import Main from "./pages/Main/Main";
+import Product from "./pages/Product/Product";
+import Detail from "./pages/Product/Detail/Detail";
+import Footer from "./components/Footer/Footer";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/detail" element={<Detail />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+};
+
+export default Router;
