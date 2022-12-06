20495771eac771f6c8566d03670cccb9ff253b11
김민서 <kimminseo@gimminseoui-MacBookPro.local>
2022-12-06 18:28:50 +0900

    router수정

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav/Nav';
-import Join from './pages/Signup/Signup';
+import Signup from './pages/Signup/Signup';
 import Login from './pages/Login/Login';
 import Main from './pages/Main/Main';
 import Product from './pages/Product/Product';
@@ -14,7 +14,7 @@ const Router = () => {
       <Nav />
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/join" element={<Join />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/product" element={<Product />} />
         <Route path="/detail" element={<Detail />} />
