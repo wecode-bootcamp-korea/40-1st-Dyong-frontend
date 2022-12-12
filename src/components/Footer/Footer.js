0c8533ce7085e29786da3e85439982eb30375972
mundayeon <mundayeon93@gmail.com>
2022-12-12 13:57:08 +0900

    Add: footer 레이아웃 구현

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,60 @@
 import React from 'react';
+import './Footer.scss';
 
 const Footer = () => {
-  return <h1>Footer</h1>;
+  return (
+    <footer className="footer">
+      {/* 첫번째섹션 */}
+      <div className="footerTopLine"></div>
+
+      {/* 두번째섹션 */}
+      <div className="footerMiddle">
+        <div className="middleLeft1">
+          <div className="title">고객상담센터 · 주문/배송문의</div>
+          <div className="number">080-123-4567</div>
+          <div className="time">평일 09:30 - 17:00 (점심 11:30 - 13:00)</div>
+        </div>
+        <div className="middleLeft2">
+          <div className="title">특판 · 대량구매 문의</div>
+          <div className="mail">help@osultea.com</div>
+          <div className="kakao">카카오톡ID : osulteamall</div>
+        </div>
+        <div className="middleRight">
+          <a href="#">
+            <img
+              src="https://www.osulloc.com/kr/ko/static_renew/images/f1.png?quality=80"
+              alt="매장안내"
+            />
+            <span>매장안내</span>
+          </a>
+          <a href="#">
+            <img
+              src="https://www.osulloc.com/kr/ko/static_renew/images/f2.png?quality=80"
+              alt="멤버십혜택"
+            />
+            <span>멤버십혜택</span>
+          </a>
+          <a href="#">
+            <img
+              src="https://www.osulloc.com/kr/ko/static_renew/images/f3.png?quality=80"
+              alt="FAQ"
+            />
+            <span>FAQ</span>
+          </a>
+          <a href="#">
+            <img
+              src="https://www.osulloc.com/kr/ko/static_renew/images/f4.png?quality=80"
+              alt="1:1문의"
+            />
+            <span>1:1문의</span>
+          </a>
+        </div>
+      </div>
+
+      {/* 세번째섹션 */}
+      <div className="footerLast"></div>
+    </footer>
+  );
 };
 
 export default Footer;
