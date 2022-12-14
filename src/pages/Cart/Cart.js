13a3b949101d21e70be27b540f2bfcb60d22a1a8
mundayeon <mundayeon93@gmail.com>
2022-12-14 10:12:32 +0900

    cart 레이아웃 수정

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -7,7 +7,24 @@ const Cart = () => {
       <section className="cartText">
         <h2>장바구니</h2>
       </section>
-      <section className="cartBox"></section>
+      <section className="cartBox">
+        <div className="leftBox">
+          <div className="section1">
+            <div className="left1">
+              <input className="checkBox" type="checkbox"></input>
+              <span className="checkBoxText">전체선택</span>
+            </div>
+            <div className="right1">
+              <span className="deleteText">선택 삭제</span>
+            </div>
+          </div>
+
+          <div className="section2"></div>
+          <div className="section3"></div>
+          <div className="section4"></div>
+        </div>
+        <div className="rightBox"></div>
+      </section>
     </main>
   );
 };
