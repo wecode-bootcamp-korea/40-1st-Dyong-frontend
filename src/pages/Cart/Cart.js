fe6a0eef3d96f897a6a2437d97becc2222202600
mundayeon <mundayeon93@gmail.com>
2022-12-14 20:49:03 +0900

    Add: 장바구니 리스트 구현

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Cart.scss';
 
 const Cart = () => {
+  const [cartsList, setCartsList] = useState(CartList);
+
+  console.log(cartsList);
+
   return (
     <main>
       <section className="cartText">
@@ -20,9 +24,41 @@ const Cart = () => {
               </button>
             </div>
           </div>
-          <div className="section2">
-            <span>장바구니에 담긴 상품이 없습니다.</span>
-          </div>
+
+          <ul className="section2">
+            {cartsList.length === 0 ? (
+              <span>장바구니에 담긴 상품이 없습니다.</span>
+            ) : (
+              cartsList.map(cartList => {
+                return (
+                  <li className="cartList" key={cartList.id}>
+                    <input
+                      className="checkBox"
+                      type="checkbox"
+                      checked={cartList.isChecked}
+                    />
+                    <div className="imgWrapper">
+                      <img src={cartList.imageUrl} alt={cartList.name} />
+                    </div>
+                    <div className="descWrapper">
+                      <strong>{cartList.name}</strong>
+                      <p>{cartList.content}</p>
+                    </div>
+                    <div className="buttonWrapper">
+                      <button className="buttonSize">-</button>
+                      <strong>{cartList.quan}</strong>
+                      <button className="buttonSize">+</button>
+                    </div>
+                    <div className="price">
+                      <strong>{cartList.price.toLocaleString()}원</strong>
+                    </div>
+                    <button className="purchaseBtn">바로구매</button>
+                  </li>
+                );
+              })
+            )}
+          </ul>
+
           <div className="section3">
             <button className="selectOrder" type="button">
               선택상품 주문
@@ -69,3 +105,38 @@ const Cart = () => {
 };
 
 export default Cart;
+
+const CartList = [
+  {
+    id: 1,
+    name: '기똥찬 녹차',
+    content: '아주 기가 막히고 코가 막히는 제주 순수 녹차',
+    imageUrl:
+      'https://www.osulloc.com/upload/kr/ko/adminImage/HB/XA/304_20211026141423508CU.png?quality=80',
+    price: 30000.0,
+    isChecked: true,
+    quan: 1,
+  },
+  {
+    id: 2,
+    name: '러블리 티 박스',
+    content:
+      '즐겁고 행복한 티타임을 선사하는 달콤하고 향긋한 오설록만의 특별한 블렌디드 티 선물 세트',
+    imageUrl:
+      'https://www.osulloc.com/upload/kr/ko/adminImage/HB/XA/304_20211026141423508CU.png?quality=80',
+    price: 20000.0,
+    isChecked: true,
+    quan: 1,
+  },
+  {
+    id: 3,
+    name: '시트릿 티 스토리',
+    content:
+      '신비로운 섬 제주의 다채로운 향기와 이야기를 품은 취향 걱정없이 선물하기 좋은 고급스러운 구성의 선물 세트',
+    imageUrl:
+      'https://www.osulloc.com/upload/kr/ko/adminImage/HB/XA/304_20211026141423508CU.png?quality=80',
+    price: 45000.0,
+    isChecked: true,
+    quan: 1,
+  },
+];
