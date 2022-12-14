4de5eb384fd6b4cc7721527d5df4b3302b4bbb51
mundayeon <mundayeon93@gmail.com>
2022-12-14 13:20:08 +0900

    Modify: cart 레이아웃 수정

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -15,15 +15,39 @@ const Cart = () => {
               <span className="checkBoxText">전체선택</span>
             </div>
             <div className="right1">
-              <span className="deleteText">선택 삭제</span>
+              <button className="deleteText" type="button">
+                선택 삭제
+              </button>
             </div>
           </div>
-
-          <div className="section2"></div>
-          <div className="section3"></div>
-          <div className="section4"></div>
+          <div className="section2">
+            <span>장바구니에 담긴 상품이 없습니다.</span>
+          </div>
+          <div className="section3">
+            <button className="selectOrder" type="button">
+              선택상품 주문
+            </button>
+            <button className="allOrder" type="button">
+              전체상품 주문하기
+            </button>
+          </div>
+          <div className="section4">
+            <img src="https://www.osulloc.com/kr/ko/static_cdj/images/presentPage/IconPresentNote.png" />
+            <span>장바구니에 보관된 상품은 3개월 후에 삭제 됩니다.</span>
+          </div>
         </div>
-        <div className="rightBox"></div>
+
+        <div className="rightBox">
+          <ul className="list">
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+          </ul>
+          <div className="price"></div>
+          <div className="order"></div>
+        </div>
       </section>
     </main>
   );
