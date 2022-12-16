3a7dcc0a71177b274ba79653ff07950aadc01d12
mundayeon <mundayeon93@gmail.com>
2022-12-16 12:15:28 +0900

    Add: fetch 추가

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,9 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Cart.scss';
 
 const Cart = () => {
-  const [cartsList, setCartsList] = useState(CartList);
+  const [loading, setLoading] = useState(true);
+  const [cartsList, setCartsList] = useState(Cart);
   const [checkItems, setCheckItems] = useState([]);
+  const [cart, setCart] = useState({});
+
+  useEffect(() => {
+    fetch('http://10.58.52.138:8000/cart/list', {
+      headers: {
+        Accept: 'application/json',
+        'Content-type': 'application/json;charset=utf-8',
+      },
+    })
+      .then(response => response.json())
+      .then(([cart]) => {
+        setCart(cart);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return null;
 
   const onClickDecreaseBtn = id => {
     const nextCartList = cartsList.map(cart => {
@@ -88,46 +106,46 @@ const Cart = () => {
             </div>
           </div>
           <ul className="section2">
-            {cartsList.length === 0 ? (
+            {cart.length === 0 ? (
               <span>장바구니에 담긴 상품이 없습니다.</span>
             ) : (
-              cartsList.map(cartList => {
+              cart.map(cart => {
                 return (
-                  <li className="cartList" key={cartList.id}>
+                  <li className="cartList" key={cart.id}>
                     <input
                       className="checkBox"
                       type="checkbox"
-                      name={`select-${cartList.id}`}
+                      name={`select-${cart.id}`}
                       onChange={e =>
-                        handleSingleCheck(e.target.checked, cartList.id)
+                        handleSingleCheck(e.target.checked, cart.id)
                       }
-                      checked={checkItems.includes(cartList.id) ? true : false}
+                      checked={checkItems.includes(cart.id) ? true : false}
                     />
                     <div className="imgWrapper">
-                      <img src={cartList.imageUrl} alt={cartList.name} />
+                      <img src={cart.image} alt={cart.name} />
                     </div>
                     <div className="descWrapper">
-                      <strong>{cartList.name}</strong>
-                      <p>{cartList.content}</p>
+                      <strong>{cart.name}</strong>
+                      <p>{cart.content}</p>
                     </div>
                     <div className="buttonWrapper">
                       <button
                         className="buttonSize"
-                        onClick={() => onClickDecreaseBtn(cartList.id)}
+                        onClick={() => onClickDecreaseBtn(cart.id)}
                       >
                         -
                       </button>
-                      <strong>{cartList.quan}</strong>
+                      <strong>{cart.quantity}</strong>
                       <button
                         className="buttonSize"
-                        onClick={() => onClickIncreaseBtn(cartList.id)}
+                        onClick={() => onClickIncreaseBtn(cart.id)}
                       >
                         +
                       </button>
                     </div>
                     <div className="price">
                       <strong>
-                        {(cartList.price * cartList.quan).toLocaleString()}원
+                        {(cart.price * cart.quantity).toLocaleString()}원
                       </strong>
                     </div>
                   </li>
@@ -164,37 +182,37 @@ const Cart = () => {
 
 export default Cart;
 
-const CartList = [
-  {
-    id: 1,
-    name: '기똥찬 녹차',
-    content: '아주 기가 막히고 코가 막히는 제주 순수 녹차',
-    imageUrl:
-      'https://www.osulloc.com/upload/kr/ko/adminImage/HB/XA/304_20211026141423508CU.png?quality=80',
-    price: 30000.0,
-    isChecked: true,
-    quan: 1,
-  },
-  {
-    id: 2,
-    name: '러블리 티 박스',
-    content:
-      '즐겁고 행복한 티타임을 선사하는 달콤하고 향긋한 오설록만의 특별한 블렌디드 티 선물 세트',
-    imageUrl:
-      'https://www.osulloc.com/upload/kr/ko/adminImage/HB/XA/304_20211026141423508CU.png?quality=80',
-    price: 20000.0,
-    isChecked: true,
-    quan: 1,
-  },
-  {
-    id: 3,
-    name: '시릿 티 스토리',
-    content:
-      '신비로운 섬 제주의 다채로운 향기와 이야기를 품은 취향 걱정없이 선물하기 좋은 고급스러운 구성의 선물 세트',
-    imageUrl:
-      'https://www.osulloc.com/upload/kr/ko/adminImage/HB/XA/304_20211026141423508CU.png?quality=80',
-    price: 45000.0,
-    isChecked: true,
-    quan: 1,
-  },
-];
+// const CartList = [
+//   {
+//     id: 1,
+//     name: '기똥찬 녹차',
+//     content: '아주 기가 막히고 코가 막히는 제주 순수 녹차',
+//     imageUrl:
+//       'https://www.osulloc.com/upload/kr/ko/adminImage/HB/XA/304_20211026141423508CU.png?quality=80',
+//     price: 30000.0,
+//     isChecked: true,
+//     quan: 1,
+//   },
+//   {
+//     id: 2,
+//     name: '러블리 티 박스',
+//     content:
+//       '즐겁고 행복한 티타임을 선사하는 달콤하고 향긋한 오설록만의 특별한 블렌디드 티 선물 세트',
+//     imageUrl:
+//       'https://www.osulloc.com/upload/kr/ko/adminImage/HB/XA/304_20211026141423508CU.png?quality=80',
+//     price: 20000.0,
+//     isChecked: true,
+//     quan: 1,
+//   },
+//   {
+//     id: 3,
+//     name: '시릿 티 스토리',
+//     content:
+//       '신비로운 섬 제주의 다채로운 향기와 이야기를 품은 취향 걱정없이 선물하기 좋은 고급스러운 구성의 선물 세트',
+//     imageUrl:
+//       'https://www.osulloc.com/upload/kr/ko/adminImage/HB/XA/304_20211026141423508CU.png?quality=80',
+//     price: 45000.0,
+//     isChecked: true,
+//     quan: 1,
+//   },
+// ];
